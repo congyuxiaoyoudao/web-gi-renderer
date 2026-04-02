9a692299c4e79aac25762a5d0afbce16629050ee
congyuxiaoyoudao <3187497704@qq.com>
2026-04-02 20:45:51 +0800

    Add github pages deploy workflow configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
   return {
+    base: mode === 'production' ? '/web-gi-renderer/' : '/',
     plugins: [react(), tailwindcss()],
     define: {},
     resolve: {
